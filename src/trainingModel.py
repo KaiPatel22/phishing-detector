6677a001024e7ec7452a08bfb3d7e4759a5af051
Kai Patel <106317141+KaiPatel22@users.noreply.github.com>
2025-07-18 13:18:04 +0100

    Using GridSearchCV which uses every combination of hyperparameters to find the best hyperparameters for the model.
    max_depth = 10 is underperforming (around 0.95 for f1 score)
    
    Produced these results on training run:
    
    Best Hyperparameters: {'max_depth': 20, 'max_features': 4, 'min_samples_leaf': 1, 'n_estimators': 100}

--- src/trainingModel.py
+++ src/trainingModel.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import GridSearchCV
 
 dataset = pd.read_csv('data/phishing.csv')
 
@@ -20,10 +21,32 @@ y_data = dataset[['class']]
 
 
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.2, random_state=42) #Splits the dataset into training and testing sets
-model = RandomForestClassifier(random_state=42)
-model.fit(x_train, y_train.values.ravel())  # Fits the model to the training data, uses ravel() to get rid of DataConversionWarning (1d array was expected)
 
-y_pred = model.predict(x_test)  # Makes predictions on the test data
+rfGrid = RandomForestClassifier() # Initializes a RandomForestClassifier object
+
+gridParams = { # Defines the hyperparameters to be tuned in the grid search, GridSearchCV will try all combinations of these parameters and record the best configuration
+    'n_estimators': [100, 200, 300],
+    'max_depth': [None, 10, 20, 30],
+    'max_features': [4, 5, 6, 7],
+    'min_samples_leaf' : [1, 2, 4],
+}
+
+grid = GridSearchCV(rfGrid, gridParams, cv=3, scoring = 'f1', verbose=3)  # Sets up GridSearchCV to find the best hyperparameters for the RandomForestClassifier
+# Test with scoring as accuracy and f1
+
+modelGrid = grid.fit(x_train, y_train.values.ravel())  # Fits the grid search to the training data, uses ravel() to get rid of DataConversionWarning (1d array was expected)
+
+# model = RandomForestClassifier(random_state=42)
+# model.fit(x_train, y_train.values.ravel())  # Fits the model to the training data, uses ravel() to get rid of DataConversionWarning (1d array was expected)
+
+# y_pred = model.predict(x_test)  # Makes predictions on the test data
+# accuracy = accuracy_score(y_test, y_pred)  # Calculates the accuracy of the model
+# print(f'Accuracy: {accuracy}')  # Prints the accuracy of the model
+# print(classification_report(y_test, y_pred))  # Prints a detailed classification report
+
+bestModel = modelGrid.best_estimator_  # Retrieves the best model from the grid search
+y_pred = bestModel.predict(x_test)  # Makes predictions on the test data
 accuracy = accuracy_score(y_test, y_pred)  # Calculates the accuracy of the model
 print(f'Accuracy: {accuracy}')  # Prints the accuracy of the model
 print(classification_report(y_test, y_pred))  # Prints a detailed classification report
+print(f'Best Hyperparameters: {modelGrid.best_params_}')  # Prints the best hyperparameters found by the grid search
